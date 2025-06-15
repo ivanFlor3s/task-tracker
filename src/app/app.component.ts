b276d570aa20ad468035bd57d0888852eae24c00
Ivan Flores <ifloresmanc96@gmail.com>
2025-06-15 19:14:37 -0300

    toggle theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'task-tracker';
+  private themeService = inject(ThemeService);
+
+  toggleDarkMode() {
+    this.themeService.toggleTheme();
+  }
 }
