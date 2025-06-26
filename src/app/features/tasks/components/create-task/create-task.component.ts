ecaa432040c02b7a9208db582c6cadbfd714bb55
Ivan Flores <ifloresmanc96@gmail.com>
2025-06-25 22:55:41 -0300

    install angular material + create task component

--- src/app/features/tasks/components/create-task/create-task.component.ts
+++ src/app/features/tasks/components/create-task/create-task.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-create-task',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule],
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.css',
 })
-export class CreateTaskComponent {}
+export class CreateTaskComponent {
+  readonly tags: string[] = ['Planeado', 'Contingencia', 'Corte'];
+}
